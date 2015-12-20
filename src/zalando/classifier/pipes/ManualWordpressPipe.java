95af5d14430f675f6a7c480ddeca7f7bfea417e7
car5ten <s59094@beuth-hochschule.de>
2015-12-20 17:53:49 +0100

    test verbesserung

--- src/zalando/classifier/pipes/ManualWordpressPipe.java
+++ src/zalando/classifier/pipes/ManualWordpressPipe.java
@@ -1,14 +1,17 @@
 package zalando.classifier.pipes;
 
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.xerces.dom.AttrNSImpl;
 import org.cyberneko.html.parsers.DOMParser;
 import org.json.simple.JSONObject;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import info.debatty.java.stringsimilarity.NormalizedLevenshtein;
 import zalando.classifier.Start;
@@ -18,6 +21,9 @@ public class ManualWordpressPipe {
 	
 	private String url;
 	private String html;
+	private ArrayList<String> unwantedTags;
+	private ArrayList<String> unwantedAtts;
+	private ArrayList<Pattern> unwantedCss;
 	
 	public ManualWordpressPipe(String url, String html) {
 		super();
@@ -25,6 +31,17 @@ public class ManualWordpressPipe {
 		this.html = html;
 		// TODO Auto-generated constructor stub
 		System.err.println("Manual Wordpress Pipe active");
+		unwantedTags = new ArrayList<>();
+		unwantedTags.add("script");
+		unwantedTags.add("#comment");
+		unwantedTags.add("style");
+		
+		unwantedAtts = new ArrayList<>();
+		unwantedAtts.add("onclick");
+		unwantedAtts.add("href");
+		
+		unwantedCss = new ArrayList<>();
+		unwantedCss.add(Pattern.compile(".*comment.*"));
 	}
 	
 	public JSONObject process()
@@ -35,6 +52,16 @@ public class ManualWordpressPipe {
 			parser.parse(is);
 			Node doc = parser.getDocument();
 			doc = this.getNodesOfInterest(doc);
+			System.out.println(this.url);
+			if (this.url.equalsIgnoreCase("http://www.manrepeller.com/2012/10/bagging.html")) {
+				int i = 1;
+			}
+			removeTags(doc);
+			if (this.url.equalsIgnoreCase("http://www.manrepeller.com/2012/10/bagging.html")) {
+				print(doc, " ");
+			}
+//			print(doc, "  ");
+			System.out.println("----");
 			
 			JSONObject goldObj = Start.gold.get(this.url);
 			if (goldObj == null) 
@@ -54,7 +81,8 @@ public class ManualWordpressPipe {
 			JSONObject obj = new JSONObject();
 			NormalizedLevenshtein nls = new NormalizedLevenshtein();
 			double lev = nls.distance(StringUtils.deleteWhitespace(titlePipe), StringUtils.deleteWhitespace(titleGold));
-			String docText = doc.getTextContent();
+			String docText = doc.getTextContent().replaceAll("\\s+", " ").replaceAll("[^\\x00-\\x7F]", "");
+			docText = StringEscapeUtils.unescapeJava(docText);
 			double jar = StringUtils.getJaroWinklerDistance(StringUtils.deleteWhitespace(docText), StringUtils.deleteWhitespace(goldObj.get("text").toString()));
 			double cosine = SimilarityUtil.consineTextSimilarity(StringUtils.split(docText), StringUtils.split(goldObj.get("text").toString()));
 			obj.put("url", this.url);
@@ -95,19 +123,38 @@ public class ManualWordpressPipe {
 	{
 		if (node.getNodeName().equalsIgnoreCase("div") || node.getNodeName().equalsIgnoreCase("article")) {
 			NamedNodeMap atts = node.getAttributes();
+			Node noi = null;
 			for (int i = 0; i < atts.getLength(); i++) {
 				Object att = atts.item(i);
 				if (att instanceof AttrNSImpl) {
 					AttrNSImpl realAtt = (AttrNSImpl)att;
-					if (realAtt.getName().equalsIgnoreCase("id")) {
-						Pattern p = Pattern.compile("post[-_]\\d+");
+					if (realAtt.getName().equalsIgnoreCase("id") ||
+						realAtt.getName().equalsIgnoreCase("class")) {
+						Pattern p = Pattern.compile("(post|article)[-_]\\d+", Pattern.CASE_INSENSITIVE);
+						Pattern p2 = Pattern.compile("(post|entry|main)[-_]?(content|body)", Pattern.CASE_INSENSITIVE);
 						if (p.matcher(realAtt.getValue()).matches()) {
+							noi = node;
+							break;
+						}
+						if (p2.matcher(realAtt.getValue()).find()) {
 							return node;
 						}
 					}
 				}
 			}
+			if (noi != null) {
+				Node childNoi = noi.getFirstChild();
+				while (childNoi != null) {
+					Node foundNoiInChild = this.getNodesOfInterest(childNoi);
+					if (foundNoiInChild != null) {
+						return foundNoiInChild;
+					}
+					childNoi = childNoi.getNextSibling();
+				}
+				return noi;
+			}
 		}
+		
 		Node noi = null;
 		Node child = node.getFirstChild();
 		while (child != null) {
@@ -134,6 +181,50 @@ public class ManualWordpressPipe {
 		return lastPart.trim();
 	}
 	
+	private void removeTags(Node node)
+	{
+		boolean deleteNode = false;
+		NamedNodeMap atts = node.getAttributes();
+		if (atts != null) {
+			for (int i = 0; i < atts.getLength(); i++) {
+				Object att = atts.item(i);
+				if (att instanceof AttrNSImpl) {
+					AttrNSImpl realAtt = (AttrNSImpl)att;
+					if (unwantedAtts.contains(realAtt.getName())) {
+						atts.removeNamedItem(realAtt.getName());
+					}
+					for (Pattern pattern : unwantedCss) {
+						if (pattern.matcher(realAtt.getNodeValue()).find()) {
+							removeSiblings(node);
+							return;
+						}
+					}
+				}
+			}
+		}
+		if (this.unwantedTags.contains(node.getNodeName().toLowerCase())) {
+			node.setTextContent("");
+		}
+		if (deleteNode) {
+			node = null;
+			return;
+		}
+		Node child = node.getFirstChild();
+        while (child != null) {
+        	removeTags(child);
+            child = child.getNextSibling();
+        }
+	}
+	
+	private void removeSiblings(Node node)
+	{
+		Node sib = node.getNextSibling();
+		if (sib != null) {
+			removeSiblings(sib);
+		}
+		node.getParentNode().removeChild(node);
+	}
+		
 	private Node cleanNode(Node node)
 	{
 		NamedNodeMap atts = node.getAttributes();
@@ -155,4 +246,13 @@ public class ManualWordpressPipe {
         }
 		return node;
 	}
+	
+	public static void print(Node node, String indent) {
+        System.out.println(indent+node.getClass().getName()+"-"+node.getNodeName());
+        Node child = node.getFirstChild();
+        while (child != null) {
+            print(child, indent+" ");
+            child = child.getNextSibling();
+        }
+    }
 }
